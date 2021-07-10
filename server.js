5fc30563f5bd8f5cf33fb027b5921f570b945649
czar28 <shashwatshrey28@gmail.com>
2021-07-10 11:30:04 +0530

    completed entire back-end

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const {check,validationResult}=require('express-validator/check');
-
 
 const app = express();
 
@@ -12,23 +10,21 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 //Init middleware
-app.use(express.json({extended:false}));
+app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => { res.send('APi is running'); })
+app.get('/', (req, res) => {
+  res.send('APi is running');
+});
 
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
 
-
 app.use('/api/auth', require('./routes/api/auth'));
-
 
 app.use('/api/profile', require('./routes/api/profile'));
 
 app.use('/api/post', require('./routes/api/post'));
 
-
 app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`);
-})
-
+  console.log(`Server started on ${PORT}`);
+});
